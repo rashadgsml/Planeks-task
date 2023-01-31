45b13dc5971599ec0282f16fbcf2537bc0f64545
User <gasimov.r.rg@gmail.com>
2023-02-01 02:05:22 +0400

    remove unnecessary comment

--- schemas/api/permissions.py
+++ schemas/api/permissions.py
@@ -2,9 +2,6 @@ from rest_framework.permissions import BasePermission
 
 
 class IsAuthor(BasePermission):
-    """
-    Allows access only to authenticated users.
-    """
 
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user
